93783a4ee0840af1964ac775c3550f1870be0e90
Connor Wiesner <wiesner.connor@gmail.com>
2024-03-27 16:36:18 -0300

    Fix nowTime object renew logic. Removed TEST_PIN from growLightSwitch module, cleaned up comments.

--- growLightSwitch.py
+++ growLightSwitch.py
@@ -2,53 +2,43 @@
 RELAY_PIN is is toggle for NO active low relay (pull pin down to close relay)
 Using RPi pin numbering (Not Broadcom)'''
 import RPi.GPIO as GPIO
-#State of lamp True=ON False=OFF
-#Relay trigger is board pin #3
+
+#Relay trigger is RPi pin 7
 RELAY_PIN = 7
-TEST_PIN = 3
+
+#Declare lamp state tracking variable
+#State of lamp True=ON False=OFF
 lampState = False
+
+#Init GPIO Interface
 GPIO.setwarnings(False)
 GPIO.setmode(GPIO.BOARD)
 GPIO.setup(RELAY_PIN, GPIO.OUT)
-GPIO.setup(TEST_PIN, GPIO.OUT)
+#Check/Set lampState
 if GPIO.input(RELAY_PIN):
     lampState = False
 else:
     lampState = True
 
-    
-#Pull relay pin low and toggle set state variable
+#Pass RPi pin to pull it low
 def relayOn(PIN):
     GPIO.output(PIN, GPIO.LOW)
-    if PIN == RELAY_PIN:
-        lampState = True
-    elif PIN == TEST_PIN:
-        testState == True
-
-#Set relay pin high and set state variable
+    
+#Pass RPi pin number to pull it high
 def relayOff(PIN):
     GPIO.output(PIN, GPIO.HIGH)
-    if PIN == RELAY_PIN:
-        lampState = False
-    elif PIN == TEST_PIN:
-        testState == False
-    #GPIO.cleanup()
     
 def lampOn():
     relayOn(RELAY_PIN)
+    lampState = True
     
 def lampOff():
     relayOff(RELAY_PIN)
+    lampState = False
     
 def lampToggle():
     if lampState:
         relayOff(RELAY_PIN)
     else:
         relayOn(RELAY_PIN)
-    
-def testToggle():
-    if testState:
-        relayOff(TEST_PIN)
-    else:
-        relayOn(TEST_PIN)
 
--- schedule.py
+++ schedule.py
@@ -2,15 +2,15 @@ import sched
 import time
 import growLightSwitch as switch
 
-#Get current time to check against the schedule
-nowTime = time.strptime(time.strftime('%Y %m %d %H:%M:%S', time.localtime()), '%Y %m %d %H:%M:%S')
-
 #Initialize schedulers for On/Off tasks
 lampOnScheduler = sched.scheduler(time.time, time.sleep)
 lampOffScheduler = sched.scheduler(time.time, time.sleep)  
 
 def startSchedule() :
     while(True):
+        #GMake time_struct object of current timem to check against the schedule
+        nowTime = time.strptime(time.strftime('%Y %m %d %H:%M:%S', time.localtime()), '%Y %m %d %H:%M:%S')
+
         #Make time_struct objects off scheduled On/Off times
         lampOnTime = time.strptime(time.strftime('%Y %m %d', time.localtime()) + ' 08:00:00', '%Y %m %d %H:%M:%S')
         lampOffTime = time.strptime(time.strftime('%Y %m %d', time.localtime()) + ' 22:30:00', '%Y %m %d %H:%M:%S')
@@ -22,12 +22,12 @@ def startSchedule() :
         #Init array of schedulers
         timeArray = [lampOnScheduler, lampOffScheduler]
         
-        #Check if the Lamp is supposed to be On/Off at the current time
-        #Modify order of scheduler array if necessary
+        #Check if the Lamp is supposed to be On. Turn on if True.
+        #If true reverse scheduler array to make next schedule lampOffScheduler
         if time.mktime(nowTime) > time.mktime(lampOnTime) and time.mktime(nowTime) < time.mktime(time.strptime('23:59:59', '%H:%M:%S')):
             switch.lampOn()
             timeArray = [lampOffScheduler, lampOnScheduler]
         
-        #Run each scheduler in order. Exit loop to reset On/Off time objects to the new date
+        #Run each scheduler in order. Exit loop to reset time time objects to the current date.
         for x in timeArray:
             x.run()
